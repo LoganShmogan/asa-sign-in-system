dad1fa56b12a18cad7ff44383427cdc09208f31e
Logan Young <younglogan09@gmail.com>
2026-03-24 11:09:22 +1300

    sorted auto reload and made sections for sign in area

--- app/app.py
+++ app/app.py
@@ -6,23 +6,35 @@ root = tk.Tk()
 
 # Window properties
 root.title("ASA Sign-in System")
-root.configure(background="white")
+root.configure(bg="white")
 root.minsize(1280, 720)
 root.geometry("300x300+50+50")
 
-# Widgets
-# Images
+# makes the window full screen borderless
+# root.attributes("-fullscreen", True)
+# root.bind("<Escape>", lambda event: root.attributes("-fullscreen", False))
+# Will have to test this
+# width = root.winfo_screenwidth()
+# height = root.winfo_screenheight()
+# root.geometry(f"{width}x{height}+0+0")
+
+
+# # Widgets
+
+# Frames
+frmEnterName = tk.Frame(root, width=800, height=720)
+frmNotices = tk.Frame(root, width=480, height=720, bg="blue")
 
 # Labels
 lblEnterName = tk.Label(root, text="Enter Name")
 lblNotices = tk.Label(root, text="Notices")
 
-# Asigned Widgets
-# Images
+# # Asigned Widgets
+# Frames
+frmEnterName.place(x=0, y=0)
+frmNotices.place(x=800, y=0)
 
 # Labels
-lblEnterName.pack()
-lblNotices.pack()
 
 # Runs inputs from user
 root.mainloop()
