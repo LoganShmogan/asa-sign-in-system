cef4ba234a35c0b89714ffff60ebb7e75d650edc
Logan Young <younglogan09@gmail.com>
2026-03-24 11:27:49 +1300

    sorted button location for admin, sorted some name areas, gave a basic look

--- app/app.py
+++ app/app.py
@@ -22,19 +22,32 @@ root.geometry("300x300+50+50")
 # # Widgets
 
 # Frames
-frmEnterName = tk.Frame(root, width=800, height=720)
-frmNotices = tk.Frame(root, width=480, height=720, bg="blue")
+frmEnterNameArea = tk.Frame(root, width=800, height=720)
+frmEnterName = tk.Frame(frmEnterNameArea, width=600, height=90, bg="purple")
+
+frmNoticesArea = tk.Frame(root, width=480, height=720, bg="blue")
 
 # Labels
 lblEnterName = tk.Label(root, text="Enter Name")
 lblNotices = tk.Label(root, text="Notices")
 
+# Buttons
+btnAdmin = tk.Button(frmEnterNameArea, text="Admin", width=20, height=1)
+
 # # Asigned Widgets
 # Frames
-frmEnterName.place(x=0, y=0)
-frmNotices.place(x=800, y=0)
+frmEnterNameArea.place(x=0, y=0)
+frmEnterName.place(x=100, y=300)
+
+frmNoticesArea.place(x=800, y=0)
 
 # Labels
+lblEnterName.place(x=300, y=270)
+
+lblNotices.place(x=1000, y=100)
+
+# Button
+btnAdmin.place(x=0, y=0)
 
 # Runs inputs from user
 root.mainloop()
